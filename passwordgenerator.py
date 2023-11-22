ddaf05d33a180ae8dd5a728f63e165a42e25d839
charank14378 <151744638+charank14378@users.noreply.github.com>
2023-11-22 17:50:42 +0530

    Add files via upload

---
+++ passwordgenerator.py
@@ -0,0 +1,11 @@
+import random
+lower='abcdefghijklmnopqrstuvwxyz'
+upper=lower.upper()
+symbols='!@#$%^&*()_=+-[]}{;:/?><.,'
+num='1234567890'
+all=lower+upper+symbols+num
+length=5
+password=''
+for i in range(length):
+    password=''.join([password,random.choice(all)])
+print("Random generated password is",password)
