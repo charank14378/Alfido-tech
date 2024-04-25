765b33f9e42db53f196f75e5e22616c197e05cb5
charank14378 <151744638+charank14378@users.noreply.github.com>
2024-04-25 20:07:23 +0530

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -5,7 +5,7 @@ symbols='!@#$%^&*()_=+-[]}{;:/?><.,'
 num='1234567890'
 all=lower+upper+symbols+num
 length=5
-password=''
+password='cherry'
 for i in range(length):
     password=''.join([password,random.choice(all)])
 print("Random generated password is",password)
